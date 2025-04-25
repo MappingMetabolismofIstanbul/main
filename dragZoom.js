ff41db38a0ce9d8d0f0ce1b32a88ffa7fc1d64f2
MappingMetabolismofIstanbul <dogatmn@gmail.com>
2025-04-25 21:51:32 +0300

    Add files via upload
    initial commit

---
+++ dragZoom.js
@@ -0,0 +1,212 @@
+﻿;
+(function(jQuery, window, document, undfined) {
+    var DragZoom = function(ele, opt) {
+        this.$element = ele;
+        this.defaults = {
+            minzoom: 1,
+            maxzoom: 5,
+            zoom: 1,
+            speed: 0.7,
+            scope: null,
+            onWheelStart: null,
+            onWheelEnd: null,
+            onDragStart: null,
+            onDragMove: null,
+            onDragEnd: null
+        };
+
+        this.options = $.extend({}, this.defaults, opt);
+        this.defaultValues = {};
+    }
+    DragZoom.prototype = {
+        Init: function() {
+            var self = this;
+            self.x = this.$element.offset().left;
+            self.y = this.$element.offset().top;
+            self.width = this.$element.width();
+            self.height = this.$element.height();
+            self.scale = 1;
+            self.relX = 0;
+            self.relY = 0;
+            self.isMoved = false;
+
+            self.defaultValues = {'x':self.x, 'y':self.y };
+
+            self.$element.on('mouseout', function(e) {
+                $('img[usemap]').css("cursor", "grab");
+                return false;
+            }).on('mousewheel', function(e, delta) {                
+                var size = delta * self.options.speed;
+                self.options.zoom = (self.options.zoom * 10 + delta) / 10;
+                self.wheel(e, self);
+                return false;
+            }).on('mousedown', function(e) {
+                $('img[usemap]').css("cursor", "grabbing");
+                self.start(e, self);
+                return false;
+            }).on('mouseup', function(e) {
+                $('img[usemap]').css("cursor", "grab");
+            });
+
+            $(document).on('mousemove', function(e) {
+                /*
+                 if (self.options.zoom > 1) {
+                     self.move(e, self);
+                 }
+                     */
+                self.move(e, self);
+                return false;
+            }).on('mouseup', function(e) {
+                self.end(e, self);
+                return false;
+            });
+            return self.$element;
+        },
+        wheel: function(ev, self) {
+            
+            if (self.options.zoom >= self.options.minzoom && self.options.zoom <= self.options.maxzoom) {
+
+                self.options.onWheelStart && typeof self.options.onWheelStart == 'function' ? self.options.onWheelStart() : null;
+
+                var cursor_x = ev.pageX,
+                    cursor_y = ev.pageY;
+
+                var eleOffset = self.$element.find('img').offset();
+                self.x = eleOffset.left;
+                self.y = eleOffset.top;
+
+                self.x = self.x - (cursor_x - self.x) * (self.options.zoom - self.scale) / self.scale;
+                self.y = self.y - (cursor_y - self.y) * (self.options.zoom - self.scale) / self.scale;
+
+                $('img[usemap]').css("cursor", self.options.zoom > self.scale ? "zoom-in" : "zoom-out");
+                self.scale = self.options.zoom;
+                
+                self.$element.find('img').offset({
+                    top: self.y,
+                    left: self.x
+                });
+                self.$element.find('canvas').offset({
+                    top: self.y,
+                    left: self.x
+                });
+                //self.$element.width(self.width * self.scale).height(self.height * self.scale);
+                //self.$element.find('img').width(self.width * self.scale).height(self.height * self.scale);
+                //self.$element.find('img').animate({ width: self.width * self.scale, height: self.height * self.scale}, 0);
+                $('img[usemap]').mapster('resize', self.width * self.scale, self.height * self.scale, 0);
+                
+                
+                
+
+                
+
+                self.options.onWheelEnd && typeof self.options.onWheelEnd == 'function' ? self.options.onWheelEnd() : null;
+            }
+            self.options.zoom = self.options.zoom < self.options.minzoom ? self.options.minzoom :
+                (self.options.zoom > self.options.maxzoom ? self.options.maxzoom : self.options.zoom);
+        },
+        start: function(ev, self) 
+        {
+            self.isMoved = true;
+            var selfOffset = self.$element.find('img').offset();
+            
+            /*
+            if(self.x == 0 && ev.clientX - selfOffset.left > 0) {
+                //.preventDefault();
+                return;
+            }
+            else if(self.y == 0 && ev.clientY - selfOffset.top) {
+                //e.preventDefault();
+                return;
+            }
+            else    
+                self.isMoved = true;
+            */
+                
+
+            self.relX = ev.clientX - selfOffset.left;
+            self.relY = ev.clientY - selfOffset.top;
+/*
+            if(self.relX > 0) {
+                self.relX = 0;
+            }
+            if(self.relY > 0) {
+                self.relY = 0;
+            }
+*/
+            self.options.onDragStart ? self.options.onDragStart() : null;
+
+        },
+        move: function(ev, self) {
+            if (self.isMoved) {
+/*
+                if(ev.clientX - self.relX > 0 && self.x < 0) {
+                    self.relX = ev.clientX;
+                }
+                if(ev.clientY - self.relY > 0 && self.y > 0) {
+                    self.relY = ev.clientY;
+                }*/
+                self.y = ev.clientY - self.relY;
+                self.x = ev.clientX - self.relX;
+
+                //self.$element.css({'top': self.y, 'left': self.x});
+                
+                self.$element.find('img').offset({
+                    top: self.y,
+                    left: self.x
+                });
+
+                // self.$element.animate({ top: self.y + 'px', left: self.x + 'px' });
+
+                self.options.onDragMove && typeof self.options.onDragMove == 'function' ? self.options.onDragMove() : null;
+                
+            }
+        },
+        end: function(ev, self) {
+            self.isMoved = false;
+
+            var offset = self.$element.find('img').offset();
+            self.$element.find('canvas').offset(offset);
+            
+            self.options.onDragEnd && typeof self.options.onDragEnd == 'function' ? self.options.onDragEnd() : null;
+        }
+    };
+
+    var dragzoom;
+    jQuery.fn.dragZoom = function(options) {
+        dragzoom = new DragZoom(this, options);
+        return dragzoom.Init();
+    }
+    jQuery.fn.dragZoomClear = function() {
+        if (dragzoom) {
+            dragzoom.options.zoom = 1;
+            dragzoom.scale = 1;
+            dragzoom.x = dragzoom.defaultValues.x;
+            dragzoom.y = dragzoom.defaultValues.y;
+            dragzoom.$element.find('img').offset({
+                top: dragzoom.y,
+                left: dragzoom.x 
+            });
+            self.isMoved = true;
+            dragzoom.end(null,dragzoom);
+            dragzoom.wheel({'pageX': 0, 'pageY': 0},dragzoom);
+        }
+    }
+    jQuery.fn.dragZoomChangeZoom = function(changeRatio) {
+        if (dragzoom) {
+            dragzoom.options.zoom = dragzoom.options.zoom + changeRatio;
+            //dragzoom.scale = dragzoom.scale + 0.2;
+            /*
+            dragzoom.x = dragzoom.defaultValues.x;
+            dragzoom.y = dragzoom.defaultValues.y;
+            dragzoom.$element.find('img').offset({
+                top: dragzoom.y,
+                left: dragzoom.x 
+            });
+            self.isMoved = true;
+            dragzoom.end(null,dragzoom);*/
+            
+            dragzoom.wheel({'pageX': dragzoom.x + (dragzoom.$element.find('img').width()/2), 'pageY': dragzoom.y + (dragzoom.$element.find('img').height()/2)},dragzoom);
+        }
+    }
+
+})($, window, document, undefined);
---
+++ map.js
@@ -0,0 +1,183 @@
+
+jQuery.fn.load = function(callback){ $(window).on("load", callback) };
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZG9nYXRtbiIsImEiOiJjbHlyMTJmcXgwMjQ3MmtzbTRsdm1pejdsIn0.vcEjL7lt8OoOPMP5o1PM6g';
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/dogatmn/cm4v9fem6000p01sf12f769y5',
+    center: [28.978001, 41.014254],
+    zoom: 11.15
+});
+
+let features = [];
+map.on('load', () => {
+    locations.forEach((location, i) => {
+        
+        map.loadImage(
+            location.icon,
+            (error, image) => {
+                if (error) 
+                    throw error;
+                if(!map.hasImage(location.icon)) {
+                    createImageBitmap(image, 0, 0, image.width, image.height, {resizeWidth: 40, resizeHeight: 40}).then(resizedImage => {
+                        map.addImage(location.icon, resizedImage);
+                    });
+                }
+            }
+        );
+
+        var feature = {
+            type: 'feature',
+            properties: {
+                description:                    
+                    '<div class="container">' +
+                    '<div class="popupHeader">' + location.title + '</div>' +  
+                    '<div class="containerInner">' +
+                    (location.image19 ?
+                        ('<div class="image-container img-container-19" onclick="bigImage(this, 19,' + i + ')" ><img class="thumbImage image19" src="' + location.image19 + '" alt="Image-19" ><div class="caption">19. Yüzyıl</div></div>') : '') +
+                    (location.image20 ?
+                        ('<div class="image-container img-container-20" onclick="bigImage(this, 20,' + i + ')" ><img class="thumbImage image20" src="' + location.image20 + '" alt="Image-20" ><div class="caption">20. Yüzyıl</div></div>') : '') +
+                    (location.image21 ?
+                        ('<div class="image-container img-container-21" onclick="bigImage(this, 21,' + i + ')" ><img class="thumbImage image21" src="' + location.image21 + '" alt="Image-21" ><div class="caption">21. Yüzyıl</div></div>') : '') +
+                    (!isEmpty(location.infoBoxVideo) ? '<iframe class="infoVideo" width="222" height="125" src="' + location.infoBoxVideo + '"></iframe>' : '') + 
+                    (!isEmpty(location.infoBoxImage) ? '<img class="infoImage" src="' + location.infoBoxImage + '" >' : '') +
+                    '<p class="infoText">' + location.infoBoxText + '</p>' +
+                    '</div>' +
+                    '<button class="icon-zoom in" onclick="zoomAction(this, 1);" type="button" aria-hidden="true">' + 
+                    '<svg width="16" height="16" fill="#ff70ac" viewBox="0 0 32 32" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M27.414,24.586l-5.077-5.077C23.386,17.928,24,16.035,24,14c0-5.514-4.486-10-10-10S4,8.486,4,14  s4.486,10,10,10c2.035,0,3.928-0.614,5.509-1.663l5.077,5.077c0.78,0.781,2.048,0.781,2.828,0  C28.195,26.633,28.195,25.367,27.414,24.586z M7,14c0-3.86,3.14-7,7-7s7,3.14,7,7s-3.14,7-7,7S7,17.86,7,14z" id="XMLID_227_"/><path d="M19,14c0,0.552-0.448,1-1,1h-3v3c0,0.552-0.448,1-1,1s-1-0.448-1-1v-3h-3c-0.552,0-1-0.448-1-1s0.448-1,1-1  h3v-3c0-0.552,0.448-1,1-1s1,0.448,1,1v3h3C18.552,13,19,13.448,19,14z" id="XMLID_230_"/></svg>' +
+                    '</button>' +
+                    '<button class="icon-zoom out" onclick="zoomAction(this, 2);" type="button" aria-hidden="true">' +
+                    '<svg width="16" height="16" fill="#ff70ac" viewBox="0 0 32 32" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M27.414,24.586l-5.077-5.077C23.386,17.928,24,16.035,24,14c0-5.514-4.486-10-10-10S4,8.486,4,14  s4.486,10,10,10c2.035,0,3.928-0.614,5.509-1.663l5.077,5.077c0.78,0.781,2.048,0.781,2.828,0  C28.195,26.633,28.195,25.367,27.414,24.586z M7,14c0-3.86,3.14-7,7-7s7,3.14,7,7s-3.14,7-7,7S7,17.86,7,14z" id="XMLID_232_"/><path d="M19,14c0,0.552-0.448,1-1,1h-8c-0.552,0-1-0.448-1-1s0.448-1,1-1h8C18.552,13,19,13.448,19,14z" id="XMLID_235_"/></svg>' +
+                    '</button>' +
+                    '<button class="icon-zoom fit" onclick="zoomAction(this, 3);" type="button" aria-hidden="true">' +
+                    '<svg width="12" height="12" fill="#ff70ac" viewBox="0 0 448 512" xmlns="http://www.w3.org/2000/svg"><path d="M128 32H32C14.31 32 0 46.31 0 64v96c0 17.69 14.31 32 32 32s32-14.31 32-32V96h64c17.69 0 32-14.31 32-32S145.7 32 128 32zM416 32h-96c-17.69 0-32 14.31-32 32s14.31 32 32 32h64v64c0 17.69 14.31 32 32 32s32-14.31 32-32V64C448 46.31 433.7 32 416 32zM128 416H64v-64c0-17.69-14.31-32-32-32s-32 14.31-32 32v96c0 17.69 14.31 32 32 32h96c17.69 0 32-14.31 32-32S145.7 416 128 416zM416 320c-17.69 0-32 14.31-32 32v64h-64c-17.69 0-32 14.31-32 32s14.31 32 32 32h96c17.69 0 32-14.31 32-32v-96C448 334.3 433.7 320 416 320z"/></svg>' +
+                    '</button>' +                    
+                    '</div>',
+
+                icon: location.icon
+            },
+            geometry: {
+                'type': 'Point',
+                'coordinates': location.coordinates
+            }
+        };
+
+        features.push(feature);
+    });
+
+
+
+    map.addSource('places', {
+        'type': 'geojson',
+        'data': {
+            'type': 'FeatureCollection',
+            'features': features            
+        }
+    });
+
+    map.addLayer({
+        'id': 'places',
+        'type': 'symbol',
+        'source': 'places',
+        'layout': {
+            'icon-image': ['get', 'icon'],
+            'icon-allow-overlap': true,
+            'icon-size': 1
+        }
+    });
+
+    map.on('click', 'places', (e) => {
+        const coordinates = e.features[0].geometry.coordinates.slice();
+        const description = e.features[0].properties.description;
+
+        if (['mercator', 'equirectangular'].includes(map.getProjection().name)) {
+            while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+                coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+            }
+        }
+
+        new mapboxgl.Popup()
+            .setLngLat(coordinates)
+            .setHTML(description)
+            .addTo(map);
+    });
+
+    map.on('mouseenter', 'places', () => {
+        map.getCanvas().style.cursor = 'pointer';
+    });
+
+    map.on('mouseleave', 'places', () => {
+        map.getCanvas().style.cursor = '';
+    });
+});
+
+var bigImage = function (e, imgIndex, locationIndex) {
+    if($(e).parents('.container').hasClass('detailView'))
+        return;
+
+    $(e).parents('.container').addClass('detailView detail' + imgIndex);
+
+    mapElement = document.createElement("map");
+    var areaIndex = 1;
+    while (locations[locationIndex]["image" + imgIndex + "_coords_" + areaIndex]) {
+        
+        mapElement.name = "image-map-" + imgIndex + "-" + areaIndex; 
+        mapElement.innerHTML += '<area target="" data-name="all" alt="" title="" href="' + locations[locationIndex]["image" + imgIndex + "_link_" + areaIndex] +
+            '" coords="' + locations[locationIndex]["image" + imgIndex + "_coords_" + areaIndex] + '" shape="poly">';
+        areaIndex++;
+    }
+
+    $(e).children('img').attr("usemap", "#" + mapElement.name);
+    $(e).parents('.container').append($(mapElement));
+
+    setTimeout(function() { 
+        $('img[usemap]').mapster({
+            fillColor: 'ff70ac',
+            fillOpacity: 0.5,
+            enableAutoResizeSupport: true,
+            autoResize: true,
+            onClick: function(e) {
+                if (e.selected) {
+                    iframe = document.createElement("iframe");
+                    $(iframe).attr('src', $(this).attr('href'));
+                    $(iframe).attr('allowfullscreen', 1);                   
+                    $(this).parents('.mapboxgl-popup').addClass('fullPage');
+                    $(this).parents('.container').addClass('externalPage');                    
+                    $(this).parents('.container').append($(iframe));
+                }           
+            }
+        });
+
+        $('area').mapster('highlight');
+        //$('area').on('mouseover', function() { 
+        //    $('img[usemap]').mapster('set', null, { fillColor: '00FF00' });  
+        //});
+
+        var defaultWidth = $('img[usemap]').width();
+        var defaultHeight = $('img[usemap]').height();
+        $('img[usemap]').parents('.containerInner').css({'max-height': defaultHeight, 'overflow': 'hidden' });
+
+        //$('img[usemap]').dragZoom();
+        $('img[usemap]').parents('.image-container').dragZoom();
+        
+       
+     }, 300);
+}
+
+var zoomAction = function (e, type) {
+    if(type == 1)
+        $('img[usemap]').dragZoomChangeZoom(0.2);
+    else if(type == 2)
+        $('img[usemap]').dragZoomChangeZoom(-0.2);
+    else if(type == 3)
+        $('img[usemap]').dragZoomClear();
+}
+
+var isEmpty = function (str) {
+    return (!str || str.length === 0 );
+}
+
+window.onresize = function(event) {
+    $('.mapboxgl-popup').remove();
+};
+
